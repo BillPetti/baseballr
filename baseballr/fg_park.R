4b7b984ef83a45bfac08f571f9f1e9a04dbab113
Bill Petti <billpetti@gmail.com>
2016-02-01 06:06:20 -0500

    Update fg_park.R

--- baseballr/fg_park.R
+++ baseballr/fg_park.R
@@ -2,7 +2,7 @@
 # yr = Year
 
 fg_park <- function(yr) {
-  factor_table <- html(paste0("http://www.fangraphs.com/guts.aspx?type=pf&teamid=0&season=", yr))
+  factor_table <- read_html(paste0("http://www.fangraphs.com/guts.aspx?type=pf&teamid=0&season=", yr))
   factor_table <- factor_table %>% html_nodes(xpath = '//*[@id="content"]/table') %>% html_table(fill = TRUE)
   factor_table <- as.data.frame(factor_table) %>% .[-(1:4), (1:14)]
   names(factor_table) <- c("season", "home_team", "basic", "single", "double", "triple", "hr", "so", "UIBB", "GB", "FB", "LD", "IFFB", "FIP")
