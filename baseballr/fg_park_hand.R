0a5a4a79873399990500aa5d4e2e97ed5c53b7b0
Bill Petti <billpetti@gmail.com>
2016-02-01 06:06:33 -0500

    Update fg_park_hand.R

--- baseballr/fg_park_hand.R
+++ baseballr/fg_park_hand.R
@@ -2,7 +2,7 @@
 # yr = Year
 
 fg_park_hand <- function(yr) {
-  factor_table <- html(paste0("http://www.fangraphs.com/guts.aspx?type=pfh&teamid=0&season=", yr))
+  factor_table <- read_html(paste0("http://www.fangraphs.com/guts.aspx?type=pfh&teamid=0&season=", yr))
   factor_table <- factor_table %>% html_nodes(xpath = '//*[@id="content"]/table') %>% html_table(fill = TRUE)
   factor_table <- as.data.frame(factor_table) %>% .[-(1:4), (1:10)]
   names(factor_table) <- c("season", "home_team", "single_as_LHH", "single_as_RHH", "double_as_LHH", "double_as_RHH", "triple_as_LHH", "triple_as_RHH", "hr_as_LHH", "hr_as_RHH")
