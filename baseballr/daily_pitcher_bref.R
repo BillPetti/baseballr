be2c70b682a71c8f2eb534317e34ee2f15da4250
Bill Petti <billpetti@gmail.com>
2016-02-02 07:04:18 -0500

    added daily pitcher results function

---
+++ baseballr/daily_pitcher_bref.R
@@ -0,0 +1,23 @@
+daily_pitcher_bref <- function(t1, t2) {
+  df <- read_html(paste0("http://www.baseball-reference.com/leagues/daily.cgi?user_team=&bust_cache=&type=p&lastndays=7&dates=fromandto&fromandto=", t1, ".", t2, "&level=mlb&franch=&stat=&stat_value=0"))
+  df <- df %>% html_nodes(xpath = '//*[@id="daily"]') %>% html_table(fill = TRUE)
+  df <- as.data.frame(df) %>% .[,-c(1,3,5)]
+  names(df)[1:4] <- c("Name", "Age", "Level", "Team")
+  df[,c(2,5:29, 36:39)] <- lapply(df[,c(2,5:29, 36:39)], as.numeric)
+  df$X1B <- with(df, H-(X2B+X3B+HR))
+  season <- substr(t1, 1, 4)
+  df$season <- season
+  df$uBB <- with(df, BB-IBB)
+  df[,30] <- as.numeric(sub("%", "", df[, 30]))
+  df[,31] <- as.numeric(sub("%", "", df[, 31]))                      
+  df[,32] <- as.numeric(sub("%", "", df[, 32])) 
+  df[,33] <- as.numeric(sub("%", "", df[, 33])) 
+  df[,34] <- as.numeric(sub("%", "", df[, 34]))
+  df[,35] <- as.numeric(sub("%", "", df[, 35]))
+  df[,c(30:35)] <- df[,c(30:35)]/100
+  df <- df[,c(41,1:13,42,14:19,40,20:39)]
+  df$Team <- gsub(" $", "", df$Team, perl=T)
+  df <- dplyr::filter(df, Name != "Name")
+  df <- arrange(df, desc(IP), desc(WHIP))
+  df
+}
