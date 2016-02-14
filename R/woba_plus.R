328f10fba4fea20893f60db616add4a903703990
Bill Petti <billpetti@gmail.com>
2016-02-14 07:33:00 -0500

    wOBA_CON
    calculating wOBA on contact has been added to the woba_plus function

--- R/woba_plus.R
+++ R/woba_plus.R
@@ -1,8 +1,8 @@
 #' Calcuate wOBA for any set of data
 #'
-#' This function allows you to calculate wOBA for any given set of data, provided the right variables are in the data set.
+#' This function allows you to calculate wOBA for any given set of data, provided the right variables are in the data set. The function currently returns both wOBA per plate appearance on wOBA per instance of fair contact.
 #'
-#' @param df A data frame of statistics that includes, at a minimum, the following columns: uBB (unintentional walks), HBP (Hit By Pitch), x1B (singles), x2B (doubles), x3B (triples), HR (home runs), AB (at-bats), SH (sacrafice hits), and season.
+#' @param df A data frame of statistics that includes, at a minimum, the following columns: uBB (unintentional walks), HBP (Hit By Pitch), x1B (singles), x2B (doubles), x3B (triples), HR (home runs), AB (at-bats), SH (sacrafice hits), SO (strike outs), and season.
 #' @keywords MLB, woba, sabermetrics
 #' @export
 
@@ -17,8 +17,9 @@ woba_plus <- function(df) {
   }
   df_join <- left_join(df, guts_table, by = "season")
   df_join$wOBA <- round((((df_join$wBB * df_join$uBB) + (df_join$wHBP * df_join$HBP) + (df_join$w1B * df_join$X1B) + (df_join$w2B * df_join$X2B) + 	(df_join$w3B * df_join$X3B) + (df_join$wHR * df_join$HR))/(df_join$AB + df_join$uBB + df_join$HBP - df_join$SH)),3)
+  df_join$wOBA_CON <- round((((df_join$w1B * df_join$X1B) + (df_join$w2B * df_join$X2B) + 	(df_join$w3B * df_join$X3B) + (df_join$wHR * df_join$HR))/(df_join$AB - df_join$SO)),3)
   df_join <- arrange_(df_join, ~desc(wOBA))
-  x <- names(df_join) %in% c("season", "lg_woba", "woba_scale", "wBB", "wHBP", "w1B", "w2B", "w3B", "wHR", "runSB", "runCS", "lg_r_pa", "lg_r_w", "cFIP")
+  x <- names(df_join) %in% c("lg_woba", "woba_scale", "wBB", "wHBP", "w1B", "w2B", "w3B", "wHR", "runSB", "runCS", "lg_r_pa", "lg_r_w", "cFIP")
   df_join <- df_join[!x]
     df_join
 }
