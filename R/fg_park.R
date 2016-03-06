9d9b23acb7ac1fe6db97d1018bfb3c59936fe4ee
Jonathan Bob <jonathan.bob@inwt-statistics.de>
2016-03-06 18:39:08 +0100

    fg_guts: replace html_nodes by html_node and locate table by xpath immediately

--- R/fg_park.R
+++ R/fg_park.R
@@ -8,12 +8,10 @@
 #' fg_park(2013)
 
 fg_park <- function(yr) {
-  factor_table <- read_html(paste0("http://www.fangraphs.com/guts.aspx?type=pf&teamid=0&season=", yr))
-  factor_table <- factor_table %>% html_nodes(xpath = '//*[@id="content"]/table') %>% html_table(fill = TRUE)
-  factor_table <- as.data.frame(factor_table)[-(1:4), (1:14)]
-  names(factor_table) <- c("season", "home_team", "basic", "single", "double", "triple", "hr", "so", "UIBB", "GB", "FB", "LD", "IFFB", "FIP")
-  for(i in c(3:14)) {
-    factor_table[,i] <- as.numeric(as.character(factor_table[,i]))
-  }
-  factor_table
+  read_html(paste0("http://www.fangraphs.com/guts.aspx?type=pf&teamid=0&season=", yr)) %>% 
+    html_node(xpath = '//*[(@id = "GutsBoard1_dg1_ctl00")]') %>% 
+    html_table %>% 
+    setNames(c("season", "home_team", "basic", "single", "double", "triple", "hr", 
+               "so", "UIBB", "GB", "FB", "LD", "IFFB", "FIP"))
+  
 }
