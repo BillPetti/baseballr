b50bf1dd9dce25071f291a9d559d6bf1f4da0957
Bill Petti <billpetti@gmail.com>
2016-03-13 08:06:21 -0400

    added html_node to NAMESPACE

--- R/fg_guts.R
+++ R/fg_guts.R
@@ -2,6 +2,7 @@
 #'
 #' This function allows you to scrape the historical, season-by-season wOBA and FIP constants and coefficients at FanGraphs.com.
 #' @keywords MLB, sabermetrics
+#' @importFrom rvest html_node
 #' @export
 #' @examples
 #' fg_guts()
@@ -10,9 +11,9 @@
 # wOBA and FIP coefficients and constants
 
 fg_guts <- function() {
-  read_html("http://www.fangraphs.com/guts.aspx?type=cn") %>% 
-    html_node(xpath = '//*[(@id = "GutsBoard1_dg1_ctl00")]') %>% 
-    html_table %>% 
-    setNames(c("season", "lg_woba", "woba_scale", "wBB", "wHBP", "w1B", "w2B", 
+  read_html("http://www.fangraphs.com/guts.aspx?type=cn") %>%
+    html_node(xpath = '//*[(@id = "GutsBoard1_dg1_ctl00")]') %>%
+    html_table %>%
+    setNames(c("season", "lg_woba", "woba_scale", "wBB", "wHBP", "w1B", "w2B",
                "w3B", "wHR", "runSB", "runCS", "lg_r_pa", "lg_r_w", "cFIP"))
 }
