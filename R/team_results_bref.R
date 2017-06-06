e4b65bb1ca87dfe5c4e7f10d1d05ae02d52219ee
Bill Petti <billpetti@gmail.com>
2017-06-06 15:44:04 -0400

    bug fixes

--- R/team_results_bref.R
+++ R/team_results_bref.R
@@ -3,7 +3,9 @@
 #' This function allows you to scrape schedule and results for a major league team from Baseball-Reference.com
 #' @param Tm The abbreviation used by Baseball-Reference.com for the team whose results you want to scrape.
 #' @param year Season for which you want to scrape the park factors.
-#' @importFrom dplyr filter_ rename
+#' @importFrom dplyr filter_ rename filter
+#' @importFrom xml2 read_html
+#' @importFrom rvest html_table
 #' @keywords MLB, sabermetrics
 #' @export
 #' @examples
@@ -12,20 +14,29 @@
 
 team_results_bref <-function(Tm, year) {
   data <- read_html(paste0("http://www.baseball-reference.com/teams/", Tm, "/", year, "-schedule-scores.shtml")) %>% html_nodes("table")
-  data <- data[[length(data)]] %>% html_table() %>% `[`(-4) %>%
-  rename(Record = `W-L`, Result = `W/L`, H_A = Opp, Gm = `Gm#`)
+  data <- data[[length(data)]] %>% html_table() %>% `[`(-3) %>%
+    rename(Record = `W-L`, Result = `W/L`, Gm = `Gm#`)
+
+  names(data) <- col_names
+
   data$H_A <- ifelse(grepl("@", data$H_A, fixed = TRUE), "A", "H")
   #data <- filter_(data, ~Rk != "Rk")
+
   data$Attendance <- gsub(",", "", data$Attendance)
+
   data$Streak <- ifelse(grepl("-", data$Streak, fixed = TRUE), nchar(data$Streak) * -1, nchar(data$Streak) * 1)
-  
+
   for (i in c("R", "Rank", "Attendance")) {
-      if(!is.numeric(data[, i])) {
-        data[,i] <- suppressWarnings(as.numeric(data[,i]))
-      }
+    if(!is.numeric(data[, i])) {
+      data[,i] <- suppressWarnings(as.numeric(data[,i]))
+    }
   }
-  
+
   data$Year <- year
   data <- data[, 1:ncol(data)]
+  data <- data %>%
+    filter(!grepl("Gm#", Gm))
+
   return(data)
 }
+
