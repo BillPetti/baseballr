c37eb22e8bd26c56f374f424756155dc706edd23
Bill Petti <billpetti@gmail.com>
2019-05-02 09:51:58 -0400

    add function for calling mlb api for draft information

---
+++ R/get_draft_mlb.R
@@ -0,0 +1,34 @@
+#' Retrieve draft pick information by year via the MLB api \url{http://statsapi.mlb.com/api/}
+#'
+#' @param year The year for which to return data
+#' @importFrom jsonlite fromJSON
+#' @importFrom dplyr bind_rows
+#' @importFrom janitor clean_names
+#' @return Returns a data frame with information for every draft pick in every
+#' round for that year
+#' requested
+#' @keywords MLB, sabermetrics
+#' @export
+#'
+#' @examples
+#' \dontrun{get_draft_mlb(2009)}
+
+get_draft_mlb <- function(year) {
+
+  api_call <- paste0("http://statsapi.mlb.com/api/v1/draft/", year)
+
+  payload <- jsonlite::fromJSON(api_call, flatten = TRUE)
+
+  draft_table <- payload$drafts$rounds$picks
+
+  draft_table <- draft_table %>%
+    dplyr::bind_rows()
+
+  columns_to_fill <- column_structure_draft_mlb
+
+  draft_table_filled <- dplyr::bind_rows(column_structure_draft_mlb,
+                                         draft_table) %>%
+    janitor::clean_names()
+
+  return(draft_table_filled)
+}
