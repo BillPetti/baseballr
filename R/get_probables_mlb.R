166fb30780698df6226979c4d32bdcc95b1e73cd
Bill Petti <billpetti@gmail.com>
2019-05-03 10:20:11 -0400

    Fix for when probable pitcher has yet to be named

--- R/get_probables_mlb.R
+++ R/get_probables_mlb.R
@@ -15,44 +15,52 @@
 #' @examples get_probables_mlb(566001)
 
 get_probables_mlb <- function(game_pk) {
-
   oldw <- getOption("warn")
   options(warn = -1)
-
   api_call <- paste0("http://statsapi.mlb.com/api/v1.1/game/", game_pk,"/feed/live")
-
   payload <- jsonlite::fromJSON(api_call)
-
-  probable <- payload$gameData$probablePitchers %>%
-    purrr::map_df(~as.data.frame(.))
-
+  away_probable <- if(is.null(payload$gameData$probablePitchers$away)) {
+    return_table <- tibble(id = NA,
+                           fullName = NA,
+                           link = NA)
+  } else {
+    return_table <- payload$gameData$probablePitchers$away %>%
+      as.data.frame()
+  }
+  home_probable <- if(is.null(payload$gameData$probablePitchers$home)) {
+    return_table <- tibble(id = NA,
+                           fullName = NA,
+                           link = NA)
+  } else {
+    return_table <- payload$gameData$probablePitchers$home %>%
+      as.data.frame()
+  }
   parse_teams <- function(payload_teams) {
-
     return_table <- tibble(team = payload_teams$name,
                            team_id = payload_teams$id)
-
     return(return_table)
   }
 
-
   away_team <- parse_teams(payload$gameData$teams$away)
-
   home_team <- parse_teams(payload$gameData$teams$home)
 
   teams <- dplyr::bind_rows(away_team,
                             home_team)
 
-  table <- dplyr::bind_cols(probable, teams)
+  probs <- dplyr::bind_rows(away_probable,
+                            home_probable)
+
+  table <- dplyr::bind_cols(probs,
+                            teams)
 
   table <- table %>%
-    dplyr::mutate(game_pk = payload$gamePk,
+    mutate(game_pk = payload$gamePk,
            game_date = stringr::str_sub(payload$gameData$game$calendarEventID,
                                         -10,
                                         -1)) %>%
-    dplyr::select(game_pk, game_date, fullName, id, team, team_id)
+    select(game_pk, game_date, fullName, id, team, team_id)
 
   options(warn = oldw)
 
   return(table)
-
 }
