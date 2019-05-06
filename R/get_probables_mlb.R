926ccd8515f7bdd4eab08c9f41639f78eb606392
Bill Petti <billpetti@gmail.com>
2019-05-06 16:21:39 -0400

    added home plate umpires to the get_probables function

--- R/get_probables_mlb.R
+++ R/get_probables_mlb.R
@@ -6,8 +6,8 @@
 #' @importFrom purrr map_df
 #' @importFrom tibble tibble
 #' @importFrom stringr str_sub
-#' @return Returns a data frame that includes probable starting pitchers for
-#' the game_pk provided
+#' @return Returns a data frame that includes probable starting pitchers and
+#' the home plate umpire for the game_pk provided
 #' requested
 #' @keywords MLB, sabermetrics
 #' @export
@@ -18,7 +18,7 @@ get_probables_mlb <- function(game_pk) {
   oldw <- getOption("warn")
   options(warn = -1)
   api_call <- paste0("http://statsapi.mlb.com/api/v1.1/game/", game_pk,"/feed/live")
-  payload <- jsonlite::fromJSON(api_call)
+  payload <- jsonlite::fromJSON(api_call, flatten = TRUE)
   away_probable <- if(is.null(payload$gameData$probablePitchers$away)) {
     return_table <- tibble(id = NA,
                            fullName = NA,
@@ -47,6 +47,20 @@ get_probables_mlb <- function(game_pk) {
   teams <- dplyr::bind_rows(away_team,
                             home_team)
 
+  if(length(payload$liveData$boxscore$officials) > 0) {
+
+    umpires <- payload$liveData$boxscore$officials %>%
+      dplyr::filter(officialType == "Home Plate") %>%
+      dplyr::rename_at(vars(contains("official")),
+                funs(sub("official", "home_plate", .))) %>%
+      dplyr::select(home_plate.id, home_plate.fullName)
+
+  } else {
+
+    umpires <- tibble(home_plate.id  = NA,
+                      home_plate.fullName = NA)
+  }
+
   probs <- dplyr::bind_rows(away_probable,
                             home_probable)
 
@@ -54,11 +68,16 @@ get_probables_mlb <- function(game_pk) {
                             teams)
 
   table <- table %>%
+    mutate(home_plate.id = umpires$home_plate.id,
+           home_plate.fullName = umpires$home_plate.fullName)
+
+  table <- table %>%
     mutate(game_pk = payload$gamePk,
            game_date = stringr::str_sub(payload$gameData$game$calendarEventID,
                                         -10,
                                         -1)) %>%
-    select(game_pk, game_date, fullName, id, team, team_id)
+    select(game_pk, game_date, fullName, id, team, team_id,
+           home_plate.fullName, home_plate.id)
 
   options(warn = oldw)
 
