cfbdf091cb6e54b84a64b427ce534b9b9a026bc6
Daniel H <darh78@me.com>
2019-08-28 00:35:33 +0100

    Correcting roxygen comments.

--- R/viz_rd.R
+++ R/viz_rd.R
@@ -1,151 +1,139 @@
-library(baseballr)
-library(tidyverse)
-#library(pbapply)
-library(teamcolors)
-#library(lubridate)
-library(highcharter)
+#' Visualize MLB team's Runs Differential
+#' 
+#' @param Tm a character object representing: Team name (BBRef), or Division, or League, or entire MLB
+#' @param year a numeric object representing the season to visualize
+#' @return This function allows to scrape MLB teams results on a specific season and visualizes the cumulative runs differential
+#' @keywords MLB, Runs, Results, Baseball, BBRef
+#' @importFrom magrittr >%>
+#' @importFrom pbapply pbapply pbsapply
+#' @importFrom tidyr separate unite
+#' @importFrom dplyr select filter group_by mutate ungroup sumarise arrange
+#' @importFrom highcharter hchart hcaes hc_tooltip hc_add_theme hc_theme_smpl hc_xAxis hc_yAxis hc_title hc_subtitle hc_credits hc_exporting hw_grid
+#' @importFrom purrr map
+#' @importFrom htmltools browsable
+#' @export viz_rd
+#' @examples
+#' viz_rd("AL East", 2019)
+#' viz_rd("BOS", 2018)
+#' viz_rd("MLB", 2000)
+#' \dontrun{
+#' viz_rd("Boston Red Sox", 2019)
+#' }
+
+# Function arguments notes:
+# 'Tm': the team acronym (used by Baseball Reference) or Division, or League or the entire MLB
+      # Other possible values for 'Tm' besides a bbref name:
+          # "MLB", "AL Overall", "NL Overall"
+          # "AL East", "AL Central", "AL West"
+          # "NL East", "NL Central", "NL West"
+# 'year' MLB Season 
 
 viz_rd <- function(Tm, year) {
   
-  # lg_div possible values: "MLB", AL", "NL", "AL East", "AL Central", "AL West", "NL East", "NL Central", "NL West"  
+  ### Identify 'Tm' input type and get names of teams to visualize
   
-  # Create a table of colors by team
-  team_palette <- Lahman::Teams %>% 
-    dplyr::filter(yearID == 2016) %>% 
-    dplyr::select(name, teamIDBR)
+  if (intersect(grepl("AL|NL", Tm),
+                grepl("East|Central|West|Overall", Tm))) { # Division or leagues in year
+    
+    teams <- standings_on_date_bref(paste0(year,"-04-30"), Tm) %>% 
+      as.data.frame() %>% 
+      select(1) %>% 
+      unlist()
+    
+  } else if (Tm == "MLB") { # All MLB teams in year
+    
+    mlb <- c("AL Overall", "NL Overall")
+    
+    print(paste0("Getting names of all MLB teams that played in ", year, "..."))
+    teams <- pbapply::pbsapply(mlb, standings_on_date_bref, date = paste0(year,"-04-30"))
+    teams <- do.call("rbind", teams) %>% 
+      select(1) %>% 
+      unlist()
   
-  team_palette$name[team_palette$teamIDBR == "LAA"] <- "Los Angeles Angels"
+  } else { # Only one team
+      teams <- Tm
+    }
   
-  team_palette <- team_palette %>% 
-    dplyr::left_join(teamcolors::teamcolors, by = "name") %>% 
-    dplyr::rename(Team = teamIDBR) %>% 
-    dplyr::select(Team, primary, secondary)
   
-  # Replace "primary" color of some teams by its "secondary" color
-  team_palette[c(1, 2, 6, 11, 16, 17, 21, 24, 27, 28), 2] <- team_palette[c(1, 2, 6, 11, 16, 17, 21, 24, 27, 28), 3]
-  # 1 ARI, 2 ATL, 6 CHC, 11 HOU, 16 MIL, 17 MIN, 21 PHI, 24 SEA, 27 TBR, 28 TEX
-  team_palette[,3] <- NULL # Remove "secondary" column 
+  print(paste0("Getting game's results data for Teams that played in ", year, " ..."))
+
+  rd <- pblapply(teams, team_results_bref, year)      # Gets the team's results tables for each team
+  rd <- do.call("rbind", rd)                          # Binds tables for all teams into one data frame
   
-  rd <- team_results_bref(Tm, year)
-  rd$Gm <- as.numeric(rd$Gm)
-  rd$RA <- as.numeric(rd$RA)
+  rd$Gm <- as.numeric(rd$Gm)                          # change Game variable to numeric
+  rd$RA <- as.numeric(rd$RA)                          # change Runs Allowed variable to numeric
+
+  ### Tidying rd data frame
   
   rd <- rd %>% 
     tidyr::separate(Date, c("wd", "Date"), sep = ", ") %>% 
     tidyr::unite(Date, c("Date", "Year"), sep = ", ") %>%
     dplyr::select(Gm, Date, Tm, Opp, R, RA) %>%
+    dplyr::filter(complete.cases(rd)) %>% 
+    dplyr::group_by(Tm) %>% 
     dplyr::mutate(R_Diff = R - RA,
                   cum_R_Diff = cumsum(R_Diff)) %>% 
-    dplyr::filter(complete.cases(rd))
+    dplyr::ungroup()
   
   names(rd)[c(1,3)] <- c("Game", "Team") 
-  
-  rd <- rd %>%
-    dplyr::left_join(team_palette, "Team")
-  
-  
-  ########## GGPLOT
-  # viz_gg <- ggplot(data = rd,
-  #                  aes(x = Game, y = cum_R_Diff)) +
-  #   geom_area(fill = team_palette$primary[team_palette$Team == Tm]) +
-  #   theme_classic() + 
-  #   labs(title = paste(Tm, "- Runs Differential"),
-  #        subtitle = paste(year, "Season"),
-  #        caption = "Source: Baseball Reference. Using 'baseballr' R package",
-  #        tag = "MLB") +
-  #   xlab("Games") +
-  #   ylab("Runs Differential")
-  # 
-  # print(viz_gg)
-  
-  ######### HIGHCHARTER
-  
-  viz_hc <- highcharter::highchart()
-  
-  viz_hc <- viz_hc %>%
-    highcharter::hc_add_series(data = rd,
-                               highcharter::hcaes(x = Game,
-                                                  y = cum_R_Diff),
-                               type = "areaspline",
-                               marker = list(enabled = FALSE),
-                               color = rd$primary,
-                               fillColor = "#D4DFD0",
-                               negativeFillColor = "#FF988C",
-                               fillOpacity = 0.1) %>%
-    highcharter::hc_legend(enabled = FALSE) %>% 
-    highcharter::hc_tooltip(useHTML = TRUE,
-                            headerFormat = "",
-                            pointFormat = "Team: {point.Team} <br>
-                            Date: {point.Date} <br>
-                            Game: {point.Game} <br>
-                            Run Diff: {point.cum_R_Diff}",
-                            borderWidth = 1,
-                            borderColor = "#000000")
-  
-  
-  viz_hc <- viz_hc %>%  
-    highcharter::hc_xAxis(title = list(text = "Games")) %>% # X axis definition
-    highcharter::hc_yAxis(title = list(text = "Runs Differential")) %>% # Y axis definition
-    highcharter::hc_title(text = paste(rd$Team[1],
-                                       "<span style=\"color:#002d73\"> - Runs Differential </span>")) %>%
-    highcharter::hc_subtitle(text = paste("Through ", year, " Season")) %>%
-    highcharter::hc_credits(enabled = TRUE, # add credits
-                            text = "Source: Baseball Reference. Using 'baseballr' R package") %>%
-    highcharter::hc_add_theme(highcharter::hc_theme_smpl())
-  
-  print(viz_hc)
-  
-  
-  ########
-  
-  opp_palette <- team_palette
-  names(opp_palette)[1] <- c("Opp")
-  
-  rd_opp <- rd %>% 
-    dplyr::group_by(Opp) %>% 
-    dplyr::summarise(R = sum(R), RA = sum(RA)) %>% 
-    dplyr::mutate(R_Diff = R - RA) %>% 
-    dplyr::left_join(opp_palette, "Opp") %>% 
-    dplyr::arrange(desc(R_Diff), desc(R)) %>% 
-    dplyr::mutate(Opp = factor(Opp, unique(Opp)))
-  
-  max_tick <- round(max(rd_opp$R, rd_opp$RA)*1.1)
-  
-  viz_opp <- highcharter::highchart()
-  
-  viz_opp <- viz_opp %>% 
-    highcharter::hc_add_series(data = rd_opp,
-                               type = "columnrange",
-                               highcharter::hcaes(x = Opp, # A factor variable
-                                                  low = -RA, # A numeric variable
-                                                  high = R,
-                                                  color = primary),
-                               name = "Runs Allowed - Scored") %>% # A numeric variable
-    hc_yAxis(tickPositions = c(-max_tick, 0, max_tick),
-             gridLineColor = "#9ca0a1",
-             title = list(text = "Runs")) %>% 
-    hc_xAxis(gridLineColor = "#ffffff",
-             labels = rd_opp$Opp,
-             title = list(text = "Opponents")) %>% 
-    highcharter::hc_legend(enabled = FALSE)
-    
-  
-  viz_opp <- viz_opp %>% 
-    highcharter::hc_add_series(data = rd_opp,
-                               type = "line",
-                               highcharter::hcaes(x = Opp,
-                                                  y = R_Diff),
-                               color = "#000000",
-                               name = "R Diff vs Opp")
 
-  viz_opp <- viz_opp %>% 
-    highcharter::hc_title(text = paste(rd$Team[1],
-                                       "<span style=\"color:#002d73\"> - Runs Differential vs Opponents </span>")) %>%
-    highcharter::hc_subtitle(text = paste("Through ", year, " Season")) %>%
-    highcharter::hc_credits(enabled = TRUE, # add credits
-                            text = "Source: Baseball Reference. Using 'baseballr' R package") %>%
-    highcharter::hc_add_theme(highcharter::hc_theme_smpl())
+  ### Determine columns for grid charts based on number of teams
   
+  if (length(teams) <= 5) {                         # Either only one team or one Division
+    viz_col <- 1
+  } else if (length(teams) == 15) {                 # One League
+    viz_col <- 3
+  } else if (length(teams) == 30) {                 # All MLB
+    viz_col <- 5
+  }
   
-    viz_opp
+  ### Defining min & max for yAxis to be the same for all charts
+  min_RDiff <- round(min(rd$cum_R_Diff)*1.1)
+  max_RDiff <- round(max(rd$cum_R_Diff)*1.1)
+  
+  ### Creating an ordered vector (not factor) of teams based on cummulated Runs Differential.
+  ### NOTE: This is because 'highcharter' hc_grid function plots charts in the order they are created and not based on factors (as ggplot) 
+  teams_factor <- rd %>%
+    dplyr::group_by(Team) %>%
+    dplyr::summarise(R = sum(R), RA = sum(RA)) %>%
+    dplyr::mutate(R_Diff = R - RA) %>%
+    dplyr::arrange(desc(R_Diff), desc(R)) %>%
+    dplyr::select(1) %>% 
+    unlist()
+
+  ###Creating charts for each team
+    
+  map(teams_factor, function(x) {
+    
+    rd[rd$Team == x,] %>% 
+      highcharter::hchart(showInLegend = FALSE,
+             type = "areaspline",
+             highcharter::hcaes(x = Game,
+                   y = cum_R_Diff),
+             marker = list(enabled = FALSE),
+             color = "#4B5463",
+             fillColor = "#D4DFD0",
+             negativeFillColor = "#FF988C",
+             fillOpacity = 0.4) %>%
+      highcharter::hc_tooltip(useHTML = TRUE,
+                 headerFormat = "",
+                 pointFormat = "Team: {point.Team} <br>
+                           Date: {point.Date} <br>
+                           Game: {point.Game} <br>
+                           Run Diff: {point.cum_R_Diff}",
+                 borderWidth = 1,
+                 borderColor = "#000000") %>%
+      highcharter::hc_add_theme(hc_theme_smpl()) %>% 
+      highcharter::hc_xAxis(title = list(text = "Games")) %>%          # X axis definition
+      highcharter::hc_yAxis(title = list(text = "R Diff"),             # Y axis definition
+               min = min_RDiff,
+               max = max_RDiff) %>%                       
+      highcharter::hc_title(text = paste(x, "<span style=\"color:#002d73\"> - Runs Differential </span>")) %>%
+      highcharter::hc_subtitle(text = paste("Through ", year, " Season")) %>%
+      highcharter::hc_credits(enabled = TRUE,                          # add credits
+                 text = "Source: Baseball Reference. Using 'baseballr' R package") %>% 
+      highcharter::hc_exporting(enabled = TRUE) }) %>%                 # enable exporting option
+    highcharter::hw_grid(rowheight = 400, ncol = viz_col)  %>%         # faceting all charts
+    browsable()
+      
 }
