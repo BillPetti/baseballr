ff22873d8ef89b73901dbb219f02d7e6b20f3aed
Bill Petti <billpetti@gmail.com>
2020-02-20 13:58:48 -0500

    Revert "player_id and url retuned in ncaa_scrape"
    This reverts commit 7b40f8b61d0676fb08e3419f7bd7da8f9ec36c79.

--- R/ncaa_scrape.R
+++ R/ncaa_scrape.R
@@ -7,10 +7,6 @@
 #' @param type A string indicating whether to return "batting" or "pitching" statistics
 #' @keywords baseball, NCAA, college
 #' @import dplyr
-#' @importFrom rvest html_table html_attr html_nodes
-#' @importFrom xml2 read_html
-#' @importFrom tibble rownames_to_column
-#' @importFrom stringr str_split
 #' @export ncaa_scrape
 #' @examples
 #' \dontrun{
@@ -23,73 +19,43 @@ ncaa_scrape <- function(teamid, year, type) {
     stop('you must provide a year that is equal to or greater than 2013')
   }
 
-  else
-    if (type == "batting") {
-      id <- subset(ncaa_season_id_lu, season == year, select = id)
-      url <- paste0("http://stats.ncaa.org/team/",teamid,"/stats?game_sport_year_ctl_id=", id, "&id=", id)
-      data_read <- xml2::read_html(url)
-      data <- data_read %>%
-        rvest::html_nodes("table") %>%
-        .[[3]] %>%
-        rvest::html_table(fill = TRUE)
-      df <- as.data.frame(data)
-      df$year <- year
-      df$teamid <- teamid
-      df <- df %>%
-        dplyr::left_join(master_ncaa_team_lu, by = c("teamid" = "school_id", "year" = "year"))
-      df <- dplyr::select(df, year, school, conference, division, everything())
-      df$Player <- gsub("x ", "", df$Player)
-      if (!"RBI2out" %in% names(df)) {
-        df$RBI2out <- NA
-      }
-
-      if('OPP DP' %in% colnames(df) == TRUE) {
-
-        df <- df %>%
-          dplyr::rename(DP = `OPP DP`)
-      }
-
-      df <- dplyr::select(df,year,school,conference,division,Jersey,Player,Yr,Pos,GP,GS,BA,OBPct,SlgPct,R,AB,H,`2B`,`3B`,TB,HR,RBI,BB,HBP,SF,SH,K,DP,CS,Picked,SB,RBI2out,teamid,conference_id)
-    }
+    else
+  if (type == "batting") {
+  id <- subset(ncaa_season_id_lu, season == year, select = id)
+  url <- paste0("http://stats.ncaa.org/team/",teamid,"/stats?game_sport_year_ctl_id=", id, "&id=", id)
+  data <- read_html(url) %>% html_nodes("table") %>%
+    .[[3]] %>%
+    html_table(fill = TRUE)
+  df <- as.data.frame(data)
+  df$year <- year
+  df$teamid <- teamid
+  df <- df %>%
+    left_join(master_ncaa_team_lu, by = c("teamid" = "school_id", "year" = "year"))
+  df <- select(df, year, school, conference, division, everything())
+  df$Player <- gsub("x ", "", df$Player)
+  if (!"RBI2out" %in% names(df)) {
+    df$RBI2out <- NA
+  }
+  df <- select(df,year,school,conference,division,Jersey,Player,Yr,Pos,GP,GS,BA,OBPct,SlgPct,R,AB,H,`2B`,`3B`,TB,HR,RBI,BB,HBP,SF,SH,K,DP,CS,Picked,SB,RBI2out,teamid,conference_id)
+  return(df)
+  }
 
   else {
     year_id <- subset(ncaa_season_id_lu, season == year, select = id)
     type_id <- subset(ncaa_season_id_lu, season == year, select = pitching_id)
     url <- paste0("http://stats.ncaa.org/team/", teamid, "/stats?id=", year_id, "&year_stat_category_id=", type_id)
-    data_read <- xml2::read_html(url)
-    data <- data_read %>%
-      rvest::html_nodes("table") %>%
+    data <- read_html(url) %>% html_nodes("table") %>%
       .[[3]] %>%
-      rvest::html_table(fill = TRUE)
+      html_table(fill = TRUE)
     df <- as.data.frame(data)
     df <- df[,-6]
     df$year <- year
     df$teamid <- teamid
     df <- df %>%
       left_join(master_ncaa_team_lu, by = c("teamid" = "school_id", "year" = "year"))
-    df <- dplyr::select(df, year, school, conference, division, everything())
+    df <- select(df, year, school, conference, division, everything())
     df$Player <- gsub("x ", "", df$Player)
-    df <- dplyr::select(df, year,school,conference,division,Jersey,Player,Yr,Pos,GP,App,GS,ERA,IP,H,R,ER,BB,SO,SHO,BF,`P-OAB`,`2B-A`,`3B-A`,Bk,`HR-A`,WP,HB,IBB,`Inh Run`,`Inh Run Score`,SHA,SFA,Pitches,GO,FO,W,L,SV,KL,teamid,conference_id)
+    df <- select(df, year,school,conference,division,Jersey,Player,Yr,Pos,GP,App,GS,ERA,IP,H,R,ER,BB,SO,SHO,BF,`P-OAB`,`2B-A`,`3B-A`,Bk,`HR-A`,WP,HB,IBB,`Inh Run`,`Inh Run Score`,SHA,SFA,Pitches,GO,FO,W,L,SV,KL,teamid,conference_id)
+    return(df)
   }
-
-  player_url <- data_read %>%
-    rvest::html_nodes('#stat_grid a') %>%
-    rvest::html_attr('href') %>%
-    as.data.frame() %>%
-    dplyr::rename(player_url = '.') %>%
-    dplyr::mutate(player_url = paste0('stats.ncaa.org', player_url))
-
-  player_id <-
-    stringr::str_split(pattern = '&stats_player_seq=',  string = player_url$player_url,simplify = T)[,2] %>%
-    as.data.frame() %>%
-    dplyr::rename(player_id = '.')
-
-  player_url_comb <- dplyr::bind_cols(player_id, player_url)
-
-  df <- dplyr::left_join(df %>%
-                           tibble::rownames_to_column('rows'),
-                         player_url_comb %>%
-                           tibble::rownames_to_column('rows'), by = 'rows')
-
-  return(df)
 }
