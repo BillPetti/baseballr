d2c89697e4cc3bf8d28fa8fe8c85c78375da9f00
Saiem Gilani <saiem.gilani@gmail.com>
2021-06-24 20:42:54 -0400

    Fixing these .[length(.)-1] things as well as the previous commit message things

--- R/milb_batter_game_logs_fg.R
+++ R/milb_batter_game_logs_fg.R
@@ -9,7 +9,7 @@
 #' @importFrom tidyr separate
 #' @export
 #' @examples
-#' \dontrun{milb_batter_game_logs_fg(playerid = "sa917940", 2018)}
+#' \dontrun{milb_batter_game_logs_fg(playerid = "sa917940", year=2018)}
 
 milb_batter_game_logs_fg <- function(playerid, year = 2017) {
 
@@ -30,40 +30,41 @@ milb_batter_game_logs_fg <- function(playerid, year = 2017) {
                     "&position=PB","&type=-2")
 
   # standard table
-  payload1 <- xml2::read_html(url_basic) %>%
-    rvest::html_nodes("table") %>%
-    .[length(.)] %>%
+  payload1 <- (xml2::read_html(url_basic) %>%
+    rvest::html_nodes("table"))[16] %>%
     rvest::html_table() %>%
     as.data.frame()
 
   payload1 <- payload1 %>%
-    dplyr::filter(!grepl("Date|Total", Date))
-
+    dplyr::filter(!grepl("Total|Postseason", .data$Season)) 
+  
   # advanced table
-  payload2 <- xml2::read_html(url_adv) %>%
-    rvest::html_nodes("table") %>%
-    .[length(.)] %>%
+  payload2 <- (xml2::read_html(url_adv) %>%
+    rvest::html_elements("table"))[17] %>%
     rvest::html_table() %>%
     as.data.frame()
 
   payload2 <- payload2 %>%
-    dplyr::filter(!grepl("Date|Total", Date)) %>%
-    dplyr::rename(BB_perc = BB., K_perc = K.,
-                wRC_plus = wRC., BB_per_K = BB.K)
+    dplyr::filter(!grepl("Total|Postseason", .data$Season),
+                  !grepl("Average", .data$Team)) 
 
-  payload2 <- as.data.frame(sapply(payload2, function(x) (gsub("\\ %", "", x))),
+  payload2 <- as.data.frame(sapply(payload2, function(x) (as.numeric(gsub("[\\%,]", "", x)))),
                            stringsAsFactors=F)
-
-  payload2$BB_perc <- as.numeric(payload2$BB_perc)/100
-  payload2$K_perc <- as.numeric(payload2$K_perc)/100
+  
+  payload2 <- payload2 %>% 
+    dplyr::rename(
+      BB_perc = .data$BB., 
+      K_perc = .data$K.,
+      BB_minus_K = .data$BB.K)
 
   # combine standard & advanced game log tabs
-  payload <- merge(payload1,payload2)
-
+  payload <- dplyr::bind_cols(payload1,payload2 %>% dplyr::select(-.data$Season,-.data$Team,-.data$AVG))
+  
   # separate Team column into Team & MiLB level
-  payload <- payload %>%
-    separate(Team, into = c("Team","Level"),sep=" ")
-
+  suppressWarnings(
+    payload <- payload %>%
+      separate(.data$Team, into = c("Team","Level"),sep=" ")
+  )
   # extract player name
 
   player_name <- xml2::read_html(url_basic) %>%
@@ -73,9 +74,10 @@ milb_batter_game_logs_fg <- function(playerid, year = 2017) {
   # add playerid to payload
 
   payload <- payload %>%
-    mutate(name = player_name,
-           minor_playerid = playerid) %>%
-    select(name, minor_playerid, everything())
+    dplyr::mutate(
+      name = player_name,
+      minor_playerid = playerid) %>%
+    dplyr::select(.data$name, .data$minor_playerid, tidyr::everything())
 
-  payload
+  return(payload)
 }
--- R/milb_pitcher_game_logs_fg.R
+++ R/milb_pitcher_game_logs_fg.R
@@ -9,63 +9,68 @@
 #' @importFrom tidyr separate
 #' @export
 #' @examples
-#' \dontrun{milb_pitcher_game_logs_fg(playerid = "sa873980", 2018)}
+#' \dontrun{milb_pitcher_game_logs_fg(playerid = "sa829043", year=2021)}
 
 milb_pitcher_game_logs_fg <- function(playerid, year = 2017) {
 
   message('Data courtey of FanGraphs.com. Please consider supporting FanGraphs by purchasing a membership: https://plus.fangraphs.com/product/fangraphs-membership/?switch-subscription=254671&item=85029&_wcsnonce=3e893e9b53&auto-switch=true')
-
+  
   # url for standard game log table
   url_basic <- paste0("http://www.fangraphs.com/statsd-legacy.aspx?playerid=",
                       playerid,
                       "&season=",
                       year,
                       "&position=P","&type=-1")
-
+  
   # url for advanced game log table
   url_adv <- paste0("http://www.fangraphs.com/statsd-legacy.aspx?playerid=",
                     playerid,
                     "&season=",
                     year,
                     "&position=P","&type=-2")
-
+  
   # standard table
-  payload1 <- xml2::read_html(url_basic) %>%
-    rvest::html_nodes("table") %>%
-    .[length(.)] %>%
+  payload1 <- (xml2::read_html(url_basic) %>%
+                 rvest::html_nodes("table"))[16] %>%
     rvest::html_table() %>%
     as.data.frame()
-
+  
   payload1 <- payload1 %>%
-    dplyr::filter(!grepl("Date|Total", Date))
-
+    dplyr::filter(!grepl("Date|Total", .data$Date))
+  
   # advanced table
-  payload2 <- xml2::read_html(url_adv) %>%
-    rvest::html_nodes("table") %>%
-    .[length(.)] %>%
+  payload2 <- (xml2::read_html(url_adv) %>%
+                 rvest::html_nodes("table"))[16] %>%
     rvest::html_table() %>%
     as.data.frame()
-
+  
   payload2 <- payload2 %>%
-    dplyr::filter(!grepl("Date|Total", Date)) %>%
-    dplyr::rename(BB_perc = BB., K_perc = K.,
-                  K_minus_BB = K.BB., LOB_perc = LOB.)
-
-  payload2 <- as.data.frame(sapply(payload2, function(x) (gsub("\\ %", "", x))),
-                            stringsAsFactors=F)
-
+    dplyr::filter(!grepl("Date|Total", .data$Date)) 
+  suppressWarnings(
+    payload2 <- as.data.frame(sapply(payload2, function(x) (as.numeric(gsub("[\\%,]", "", x)))),
+                              stringsAsFactors=F)
+  )
+  payload2 <- payload2 %>% 
+    dplyr::rename(
+      BB_perc = .data$BB., 
+      K_perc = .data$K.,
+      K_minus_BB = .data$K.BB., 
+      LOB_perc = .data$LOB.)
   payload2$BB_perc <- as.numeric(payload2$BB_perc)/100
   payload2$K_perc <- as.numeric(payload2$K_perc)/100
   payload2$K_minus_BB <- as.numeric(payload2$K_minus_BB)/100
   payload2$LOB_perc <- as.numeric(payload2$LOB_perc)/100
 
   # combine standard & advanced game log tables
-  payload <- merge(payload1,payload2)
+  payload <- dplyr::bind_cols(payload1 %>% dplyr::select(-.data$ERA),
+                   payload2 %>% 
+                     dplyr::select(-.data$Date,-.data$Team,-.data$Opp,-.data$GS))
 
   # separate Team column into Team & MiLB level
+  suppressWarnings(
   payload <- payload %>%
-    separate(Team, into = c("Team","Level"),sep=" ")
-
+    separate(.data$Team, into = c("Team","Level"),sep=" ")
+  )
   # extract player name
 
   player_name <- xml2::read_html(url_basic) %>%
@@ -75,9 +80,19 @@ milb_pitcher_game_logs_fg <- function(playerid, year = 2017) {
   # add playerid to payload
 
   payload <- payload %>%
-    mutate(name = player_name,
-           minor_playerid = playerid) %>%
-    select(name, minor_playerid, everything())
-
-  payload
+    dplyr::mutate(
+      name = player_name,
+      minor_playerid = playerid) %>%
+    dplyr::select(.data$name, .data$minor_playerid, tidyr::everything())
+  numeric_cols <- c("GS", "W", "L", "CG", "ShO", "SV", "IP", 
+                    "TBF", "H", "R", "ER", "HR", "BB", "IBB", 
+                    "HBP", "WP", "BK", "SO", "K.9", "BB.9", "K.BB", 
+                    "HR.9", "K_perc", "BB_perc", "K_minus_BB", 
+                    "AVG", "WHIP", "BABIP", "LOB_perc", "ERA", "FIP")
+  
+  suppressWarnings(
+    payload <- payload %>% 
+      dplyr::mutate_at(numeric_cols,as.numeric)
+  )
+  return(payload)
 }
