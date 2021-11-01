f19b3b9e95fe7a40e827c4a21481f41755193c85
Daniel Amaral <silva.daniel86@Gmail.com>
2021-11-01 14:08:22 -0300

    add if clause for non-game days handling

--- R/get_game_pks_mlb.R
+++ R/get_game_pks_mlb.R
@@ -32,9 +32,36 @@ get_game_pks_mlb <- function(date,
 
   payload <- jsonlite::fromJSON(api_call, flatten = TRUE)
 
-  payload <- payload$dates$games %>%
-    as.data.frame() %>%
-    rename(game_pk = .data$gamePk)
+  if(payload$totalGames == 0) {
+    cols <- c(
+      "game_pk", "link", "gameType", "season", "gameDate",
+      "officialDate", "isTie", "gameNumber", "publicFacing",
+      "doubleHeader", "gamedayType", "tiebreaker",
+      "calendarEventID", "seasonDisplay", "dayNight",
+      "scheduledInnings", "reverseHomeAwayStatus",
+      "inningBreakLength", "gamesInSeries", "seriesGameNumber",
+      "seriesDescription", "recordSource", "ifNecessary",
+      "ifNecessaryDescription", "status.abstractGameState",
+      "status.codedGameState", "status.detailedState",
+      "status.statusCode", "status.startTimeTBD",
+      "status.abstractGameCode", "teams.away.score",
+      "teams.away.isWinner", "teams.away.splitSquad",
+      "teams.away.seriesNumber", "teams.away.leagueRecord.wins",
+      "teams.away.leagueRecord.losses", "teams.away.leagueRecord.pct",
+      "teams.away.team.id", "teams.away.team.name", "teams.away.team.link",
+      "teams.home.score", "teams.home.isWinner", "teams.home.splitSquad",
+      "teams.home.seriesNumber", "teams.home.leagueRecord.wins",
+      "teams.home.leagueRecord.losses", "teams.home.leagueRecord.pct",
+      "teams.home.team.id", "teams.home.team.name", "teams.home.team.link",
+      "venue.id", "venue.name", "venue.link", "content.link"
+    )
+    payload <- data.frame(matrix(ncol = length(cols), nrow = 0))
+    colnames(payload) <- cols
+  } else {
+    payload <- payload$dates$games %>%
+      as.data.frame() %>%
+      rename(game_pk = .data$gamePk)
+  }
 
   return(payload)
 
