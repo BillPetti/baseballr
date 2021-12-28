b6289056703b52881e7bcdf32e47cbc0f4e2376c
saiemgilani <saiem.gilani@gmail.com>
2021-12-28 18:49:45 -0500

    Create mlb_league_leader_types.R

---
+++ R/mlb_league_leader_types.R
@@ -0,0 +1,27 @@
+#' @title **MLB League Leader Types** 
+#' @return Returns a tibble with the following columns
+#'  |col_name      |types     |
+#'  |:-------------|:---------|
+#'  |leader_type   |character |
+#' @export
+#' @examples \donttest{
+#'   mlb_league_leader_types()
+#' }
+mlb_league_leader_types <- function(){
+  
+  mlb_endpoint <- mlb_stats_endpoint("v1/leagueLeaderTypes")
+  query_params <- list()
+  
+  mlb_endpoint <- httr::modify_url(mlb_endpoint, query = query_params)
+  
+  resp <- mlb_endpoint %>% 
+    mlb_api_call()
+  league_leader_types <- jsonlite::fromJSON(jsonlite::toJSON(resp), flatten = TRUE)  %>% 
+    janitor::clean_names() %>% 
+    as.data.frame() %>% 
+    dplyr::rename(
+      leader_type = .data$display_name)
+  
+  return(league_leader_types)
+}
+
