c096c7ecaa1f4f9f2c70b2280f02729d6de2089a
saiemgilani <saiem.gilani@gmail.com>
2021-12-28 20:28:19 -0500

    Create mlb_stat_groups.R

---
+++ R/mlb_stat_groups.R
@@ -0,0 +1,28 @@
+#' @title **MLB Stat Groups** 
+#' @return Returns a tibble with the following columns
+#'  |col_name               |types     |
+#'  |:----------------------|:---------|
+#'  |stat_group_name        |character |
+#' @export
+#' @examples \donttest{
+#'   mlb_stat_groups()
+#' }
+mlb_stat_groups <- function(){
+  
+  mlb_endpoint <- mlb_stats_endpoint("v1/statGroups")
+  query_params <- list()
+  
+  mlb_endpoint <- httr::modify_url(mlb_endpoint, query = query_params)
+  
+  resp <- mlb_endpoint %>% 
+    mlb_api_call()
+  stat_groups <- jsonlite::fromJSON(jsonlite::toJSON(resp), flatten = TRUE)  %>% 
+    janitor::clean_names() %>% 
+    as.data.frame() %>% 
+    dplyr::rename(
+      stat_group_name = .data$display_name
+    )
+  
+  return(stat_groups)
+}
+
