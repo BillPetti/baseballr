e990b03e4403ac2dfc0d2c3148bc27c3abd8d93b
saiemgilani <saiem.gilani@gmail.com>
2021-12-28 20:20:05 -0500

    Create mlb_situation_codes.R

---
+++ R/mlb_situation_codes.R
@@ -0,0 +1,36 @@
+#' @title **MLB Situation Codes** 
+#' @return Returns a tibble with the following columns
+#'  |col_name                   |types     |
+#'  |:--------------------------|:---------|
+#'  |situation_code             |character |
+#'  |sort_order                 |integer   |
+#'  |navigation_menu            |character |
+#'  |situation_code_description |character |
+#'  |team                       |logical   |
+#'  |batting                    |logical   |
+#'  |fielding                   |logical   |
+#'  |pitching                   |logical   |
+#' @export
+#' @examples \donttest{
+#'   mlb_situation_codes()
+#' }
+mlb_situation_codes <- function(){
+  
+  mlb_endpoint <- mlb_stats_endpoint("v1/situationCodes")
+  query_params <- list()
+  
+  mlb_endpoint <- httr::modify_url(mlb_endpoint, query = query_params)
+  
+  resp <- mlb_endpoint %>% 
+    mlb_api_call()
+  situation_codes <- jsonlite::fromJSON(jsonlite::toJSON(resp), flatten = TRUE)  %>% 
+    janitor::clean_names() %>% 
+    as.data.frame() %>% 
+    dplyr::rename(
+      situation_code = .data$code,
+      situation_code_description = .data$description
+    )
+  
+  return(situation_codes)
+}
+
