244c270057166af80fa1e4f2bdb327b26e8a351f
saiemgilani <saiem.gilani@gmail.com>
2021-12-28 20:26:17 -0500

    Create mlb_standings_types.R

---
+++ R/mlb_standings_types.R
@@ -0,0 +1,30 @@
+#' @title **MLB Standings Types** 
+#' @return Returns a tibble with the following columns
+#'  |col_name                   |types     |
+#'  |:--------------------------|:---------|
+#'  |standings_type_name        |character |
+#'  |standings_type_description |character |
+#' @export
+#' @examples \donttest{
+#'   mlb_standings_types()
+#' }
+mlb_standings_types <- function(){
+  
+  mlb_endpoint <- mlb_stats_endpoint("v1/standingsTypes")
+  query_params <- list()
+  
+  mlb_endpoint <- httr::modify_url(mlb_endpoint, query = query_params)
+  
+  resp <- mlb_endpoint %>% 
+    mlb_api_call()
+  standings_types <- jsonlite::fromJSON(jsonlite::toJSON(resp), flatten = TRUE)  %>% 
+    janitor::clean_names() %>% 
+    as.data.frame() %>% 
+    dplyr::rename(
+      standings_type_name = .data$name,
+      standings_type_description = .data$description,
+    )
+  
+  return(standings_types)
+}
+
