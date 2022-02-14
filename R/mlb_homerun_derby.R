14c39c4c4862b644eee590a3275abc6784206eed
saiemgilani <saiem.gilani@gmail.com>
2022-02-14 09:50:05 -0500

    Update mlb_homerun_derby.R

--- R/mlb_homerun_derby.R
+++ R/mlb_homerun_derby.R
@@ -137,7 +137,39 @@ mlb_homerun_derby <- function(
       .data$batter, 
       .data$batter_id, 
       .data$batter_link, 
-      tidyr::everything())
+      tidyr::everything()) %>% 
+    dplyr::select(c(
+        "game_pk", "event_name", "event_date", 
+        "event_type_code", "event_type_name",
+        "venue_id", "venue_name", "round",
+        "num_batters", "batter", "batter_id", 
+        "batter_link", "top_seed_complete", 
+        "top_seed_started", "top_seed_winner",
+        "bonus_time", "home_run", "tie_breaker",
+        "is_home_run", "time_remaining",
+        "is_bonus_time", "is_tie_breaker", 
+        "hit_data_launch_speed", "hit_data_launch_angle",
+        "hit_data_total_distance", "hit_data_coordinates_coord_x",
+        "hit_data_coordinates_coord_y", "hit_data_coordinates_landing_pos_x",
+        "hit_data_coordinates_landing_pos_y", 
+        "hit_data_trajectory_data_trajectory_polynomial_x", 
+        "hit_data_trajectory_data_trajectory_polynomial_y",
+        "hit_data_trajectory_data_trajectory_polynomial_z", 
+        "hit_data_trajectory_data_valid_time_interval",
+        "top_seed_seed", "top_seed_is_winner", "top_seed_is_complete",
+        "top_seed_is_started", "top_seed_num_home_runs", 
+        "top_seed_player_id", "top_seed_player_full_name",
+        "top_seed_player_link", "top_seed_top_derby_hit_data_launch_speed",
+        "top_seed_top_derby_hit_data_total_distance", "bottom_seed_complete",
+        "bottom_seed_started", "bottom_seed_winner", "bottom_seed_seed",
+        "bottom_seed_is_winner", "bottom_seed_is_complete",
+        "bottom_seed_is_started", "bottom_seed_num_home_runs", 
+        "bottom_seed_player_id", "bottom_seed_player_full_name", 
+        "bottom_seed_player_link", "bottom_seed_top_derby_hit_data_launch_speed",
+        "bottom_seed_top_derby_hit_data_total_distance", 
+        "venue_link", "is_multi_day", 
+        "is_primary_calendar", "file_code", "event_number",
+        "public_facing"))
   return(rounds_hits)
 }
 #' @rdname mlb_homerun_derby_bracket
@@ -225,7 +257,27 @@ mlb_homerun_derby_bracket <- function(
   
   bracket <- info %>% 
     dplyr::bind_cols(rounds) %>% 
-    dplyr::select(-.data$top_seed_hits, -.data$bottom_seed_hits)
+    dplyr::select(-.data$top_seed_hits, -.data$bottom_seed_hits) %>% 
+    dplyr::select(c(
+      "game_pk", "event_name", "event_type_code",
+      "event_type_name", "event_date", "venue_id",
+      "venue_name", "venue_link", "is_multi_day",
+      "is_primary_calendar", "file_code", "event_number",
+      "public_facing", "round", "num_batters",
+      "top_seed_complete", "top_seed_started", 
+      "top_seed_winner", "top_seed_seed", 
+      "top_seed_is_winner", "top_seed_is_complete",
+      "top_seed_is_started", "top_seed_num_home_runs",
+      "top_seed_player_id", "top_seed_player_full_name", 
+      "top_seed_player_link", "top_seed_top_derby_hit_data_launch_speed", 
+      "top_seed_top_derby_hit_data_total_distance", "bottom_seed_complete",
+      "bottom_seed_started", "bottom_seed_winner", "bottom_seed_seed",
+      "bottom_seed_is_winner", "bottom_seed_is_complete",
+      "bottom_seed_is_started", "bottom_seed_num_home_runs", 
+      "bottom_seed_player_id", "bottom_seed_player_full_name",
+      "bottom_seed_player_link", "bottom_seed_top_derby_hit_data_launch_speed",
+      "bottom_seed_top_derby_hit_data_total_distance"
+    ))
   
   return(bracket)
 }
@@ -374,7 +426,64 @@ mlb_homerun_derby_players <- function(
       .data$event_type_name,
       .data$venue_id,
       .data$venue_name,
-      tidyr::everything())
+      tidyr::everything()) %>% 
+    dplyr::select(c(
+      "game_pk", "event_name", "event_date", 
+      "event_type_code", "event_type_name", 
+      "venue_id", "venue_name", "player_id",
+      "player_full_name", "player_link",
+      "player_first_name", "player_last_name", 
+      "player_primary_number", "player_birth_date",
+      "player_current_age", "player_birth_city", 
+      "player_birth_state_province", "player_birth_country",
+      "player_height", "player_weight", "player_active", 
+      "player_use_name", "player_middle_name", "player_boxscore_name",
+      "player_nick_name", "player_gender", "player_is_player", 
+      "player_is_verified", "player_draft_year", "player_pronunciation",
+      "player_mlb_debut_date", "player_name_first_last", 
+      "player_name_slug", "player_first_last_name",
+      "player_last_first_name", "player_last_init_name", 
+      "player_init_last_name", "player_full_fml_name", 
+      "player_full_lfm_name", "player_strike_zone_top", 
+      "player_strike_zone_bottom", "player_name_matrilineal",
+      "player_current_team_id", "player_current_team_name", 
+      "player_current_team_link", "player_current_team_season",
+      "player_current_team_team_code", "player_current_team_file_code", 
+      "player_current_team_abbreviation", "player_current_team_team_name",
+      "player_current_team_location_name",
+      "player_current_team_first_year_of_play", 
+      "player_current_team_short_name", 
+      "player_current_team_franchise_name",
+      "player_current_team_club_name", 
+      "player_current_team_all_star_status",
+      "player_current_team_active", 
+      "player_current_team_parent_org_name", 
+      "player_current_team_parent_org_id",
+      "player_current_team_venue_id", 
+      "player_current_team_venue_name", 
+      "player_current_team_venue_link", 
+      "player_current_team_spring_venue_id",
+      "player_current_team_spring_venue_link",
+      "player_current_team_league_id", 
+      "player_current_team_league_name", 
+      "player_current_team_league_link", 
+      "player_current_team_division_id",
+      "player_current_team_division_name",
+      "player_current_team_division_link",
+      "player_current_team_sport_id",
+      "player_current_team_sport_link", 
+      "player_current_team_sport_name",
+      "player_current_team_spring_league_id",
+      "player_current_team_spring_league_name", 
+      "player_current_team_spring_league_link",
+      "player_current_team_spring_league_abbreviation", 
+      "player_primary_position_code", "player_primary_position_name", 
+      "player_primary_position_type", "player_primary_position_abbreviation",
+      "player_bat_side_code", "player_bat_side_description",
+      "player_pitch_hand_code", "player_pitch_hand_description",
+      "venue_link", "is_multi_day", 
+      "is_primary_calendar", "file_code", "event_number", "public_facing"
+    ))
   return(players)
 }
 
