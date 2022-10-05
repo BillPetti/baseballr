85ac3c7d52b284e6c346b4ec2b1e6ef567be3efa
Hadley Wickham <h.wickham@gmail.com>
2022-10-05 12:26:51 -0500

    Fix for dev tidyselect (#269)

--- R/ncaa_scrape.R
+++ R/ncaa_scrape.R
@@ -61,7 +61,7 @@ ncaa_scrape <- function(teamid, year, type = 'batting') {
       if (type == "batting") {
         id <- baseballr::ncaa_season_id_lu %>% 
           dplyr::filter(.data$season == year) %>% 
-          dplyr::select(.data$id)
+          dplyr::select("id")
         
         url <- paste0("http://stats.ncaa.org/team/",teamid,"/stats?game_sport_year_ctl_id=", id, "&id=", id)
         data_read <- xml2::read_html(url)
@@ -75,23 +75,22 @@ ncaa_scrape <- function(teamid, year, type = 'batting') {
           dplyr::left_join(baseballr::ncaa_team_lu,
                            by = c("teamid" = "school_id", "year" = "year"))
         df <- df %>% 
-          dplyr::select(.data$year, .data$school, .data$conference, .data$division, tidyr::everything())
+          dplyr::select("year", "school", "conference", "division", tidyr::everything())
         df$Player <- gsub("x ", "", df$Player)
         if (!"RBI2out" %in% names(df)) {
           df$RBI2out <- NA
         }
         
         if('OPP DP' %in% colnames(df) == TRUE) {
-          
           df <- df %>%
-            dplyr::rename(DP = .data$`OPP DP`)
+            dplyr::rename(DP = "OPP DP")
         }
         
         df <- df %>% dplyr::select(
-          .data$year,.data$school,.data$conference,.data$division,.data$Jersey,.data$Player,
-          .data$Yr,.data$Pos,.data$GP,.data$GS,.data$BA,.data$OBPct,.data$SlgPct,.data$R,.data$AB,
-          .data$H,.data$`2B`,.data$`3B`,.data$TB,.data$HR,.data$RBI,.data$BB,.data$HBP,.data$SF,.data$SH,
-          .data$K,.data$DP,.data$CS,.data$Picked,.data$SB,.data$RBI2out,.data$teamid,.data$conference_id)
+          "year","school","conference","division","Jersey","Player",
+          "Yr","Pos","GP","GS","BA","OBPct","SlgPct","R","AB",
+          "H","2B","3B","TB","HR","RBI","BB","HBP","SF","SH",
+          "K","DP","CS","Picked","SB","RBI2out","teamid","conference_id")
         
         character_cols <- c("year", "school", "conference", "Jersey", "Player",
                             "Yr", "Pos")
@@ -112,10 +111,10 @@ ncaa_scrape <- function(teamid, year, type = 'batting') {
       } else {
         year_id <- baseballr::ncaa_season_id_lu %>% 
           dplyr::filter(.data$season == year) %>% 
-          dplyr::select(.data$id)
+          dplyr::select("id")
         type_id <- baseballr::ncaa_season_id_lu %>% 
           dplyr::filter(.data$season == year) %>% 
-          dplyr::select(.data$pitching_id)
+          dplyr::select("pitching_id")
         url <- paste0("http://stats.ncaa.org/team/", teamid, "/stats?id=", year_id, "&year_stat_category_id=", type_id)
         data_read <- xml2::read_html(url)
         data <- (data_read %>%
@@ -128,17 +127,17 @@ ncaa_scrape <- function(teamid, year, type = 'batting') {
         df <- df %>%
           dplyr::left_join(baseballr::ncaa_team_lu, by = c("teamid" = "school_id", "year" = "year"))
         df <- df %>% 
-          dplyr::select(.data$year, .data$school, .data$conference, .data$division, tidyr::everything())
+          dplyr::select("year", "school", "conference", "division", tidyr::everything())
         df$Player <- gsub("x ", "", df$Player)
         
         df <- df %>% dplyr::select( 
-          .data$year,.data$school,.data$conference,.data$division,.data$Jersey,.data$Player,
-          .data$Yr,.data$Pos,.data$GP,.data$App,.data$GS,.data$ERA,.data$IP,.data$H,.data$R,
-          .data$ER,.data$BB,.data$SO,.data$SHO,.data$BF,.data$`P-OAB`,
-          .data$`2B-A`,.data$`3B-A`,.data$Bk,.data$`HR-A`,.data$WP,.data$HB,
-          .data$IBB,.data$`Inh Run`,.data$`Inh Run Score`,
-          .data$SHA,.data$SFA,.data$Pitches,.data$GO,.data$FO,.data$W,.data$L,
-          .data$SV,.data$KL,.data$teamid,.data$conference_id)
+          "year","school","conference","division","Jersey","Player",
+          "Yr","Pos","GP","App","GS","ERA","IP","H","R",
+          "ER","BB","SO","SHO","BF","P-OAB",
+          "2B-A","3B-A","Bk","HR-A","WP","HB",
+          "IBB","Inh Run","Inh Run Score",
+          "SHA","SFA","Pitches","GO","FO","W","L",
+          "SV","KL","teamid","conference_id")
         
         character_cols <- c("year", "school", "conference", "Jersey", "Player",
                             "Yr", "Pos")
@@ -161,21 +160,21 @@ ncaa_scrape <- function(teamid, year, type = 'batting') {
         html_elements('#stat_grid a') %>%
         html_attr('href') %>%
         as.data.frame() %>%
-        dplyr::rename(player_url = .data$`.`) %>%
+        dplyr::rename(player_url = ".") %>%
         dplyr::mutate(player_url = paste0('http://stats.ncaa.org', .data$player_url))
       
       player_names_join <- data_read %>%
         html_elements('#stat_grid a') %>%
         html_text() %>%
         as.data.frame() %>%
-        dplyr::rename(player_names_join = .data$`.`)
+        dplyr::rename(player_names_join = ".")
       
       player_id <-
         stringr::str_split(
           pattern = '&stats_player_seq=',  
           string = player_url$player_url, simplify = TRUE)[,2] %>%
         as.data.frame() %>%
-        dplyr::rename(player_id = .data$`.`)
+        dplyr::rename(player_id = ".")
       
       player_url_comb <- dplyr::bind_cols(player_names_join, player_id, player_url)
       
@@ -190,8 +189,6 @@ ncaa_scrape <- function(teamid, year, type = 'batting') {
     },
     error = function(e) {
       message(glue::glue("{Sys.time()}: Invalid arguments provided"))
-    },
-    warning = function(w) {
     },
     finally = {
     }
