201e7641a0dbf3bc19fee6b35fa726fa4fdb4302
saiemgilani <saiem.gilani@gmail.com>
2023-02-16 06:18:43 -0500

    Remove `ncaa_team_lu` and `ncaa_season_id_lu` package data

--- R/data.R
+++ R/data.R
@@ -1,36 +1,3 @@
-#' @name ncaa_season_id_lu
-#' @title **A data set of college baseball seasons**
-#' @description A data set college baseball
-#' @docType data
-#' @keywords datasets
-#' @usage data(ncaa_season_id_lu)
-#' @format A data frame with 4 variables:
-#' \itemize{
-#'   \item \code{season}: Year date
-#'   \item \code{id}: Season id
-#'   \item \code{batting_id}: Batting id
-#'   \item \code{pitching_id}: Pitching id
-#' }
-"ncaa_season_id_lu"
-
-#' @name ncaa_team_lu
-#' @title **A data set colleges and their athletic conferences and divisions**
-#' @description A data set colleges and their athletic conferences and divisions keyed by school and year.
-#' @docType data
-#' @keywords datasets
-#' @usage data(ncaa_team_lu)
-#' @format A data frame with 6 variables
-#' \itemize{
-#'   \item \code{school}: School name
-#'   \item \code{conference}: Conference name
-#'   \item \code{school_id}: School id
-#'   \item \code{year}: Year date
-#'   \item \code{division}: School division
-#'   \item \code{conference_id}: School conference id
-#' }
-#' 
-"ncaa_team_lu"
-
 #' @name column_structure_draft_mlb
 #' @title  **Column structure of the MLB Draft data**
 #' @description A tibble giving column structure of MLB Draft data
