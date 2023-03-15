88777df2ba1e1710a5d2cfa4899d757a40b8a0c4
saiemgilani <saiem.gilani@gmail.com>
2023-03-15 14:12:15 -0400

    Update test-mlb_pitch_codes.R

--- tests/testthat/test-mlb_pitch_codes.R
+++ tests/testthat/test-mlb_pitch_codes.R
@@ -1,6 +1,15 @@
 
 cols <- c(
-  "pitch_code", "pitch_description"
+  "pitch_code",
+  "pitch_description",
+  "swing_status",
+  "swing_miss_status",
+  "swing_contact_status",
+  "sort_order",
+  "strike_status",
+  "ball_status",
+  "pitch_status",
+  "pitch_result_text"
 )
 
 test_that("MLB Pitch Codes", {
