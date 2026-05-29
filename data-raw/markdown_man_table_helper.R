02293d90a064652b4ff930829496babb23f95fdd
saiemgilani <saiem.gilani@gmail.com>
2026-05-29 05:59:18 -0400

    chore(dev): add testy() formals-inspection helper
    Assigns a function's default formals into the global environment to speed up
    interactive debugging of wrapper functions.

--- data-raw/markdown_man_table_helper.R
+++ data-raw/markdown_man_table_helper.R
@@ -3,3 +3,16 @@ df <- data.frame(col_name = colnames(x), types = sapply(x, class))
 df %>% dplyr::select(col_name, types) %>% knitr::kable(row.names = FALSE)
 
 x %>% knitr::kable()
+
+testy <- function(func) {
+  # Extract the parameters of the given function
+  params <- formals(func)
+
+  # Loop through each parameter and assign it to the global environment
+  for (param_name in names(params)) {
+    # Use 'assign' to put each parameter in the global environment
+    assign(param_name, eval(params[[param_name]]), envir = .GlobalEnv)
+  }
+
+  cat("Parameters have been assigned to the global environment.\n")
+}
